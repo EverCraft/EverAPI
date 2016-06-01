613ecfffeb42f16d74e7a49905ca055abfb44cbf
rexbut <butbutdu45@gmail.com>
2016-06-02 00:06:24 +0200

    Update
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/everapi/services/bossbar/BossBarService.java
+++ src/main/java/fr/evercraft/everapi/services/bossbar/BossBarService.java
@@ -1,5 +1,7 @@
 package fr.evercraft.everapi.services.bossbar;
 
+import java.util.Optional;
+
 import org.spongepowered.api.boss.ServerBossBar;
 
 import fr.evercraft.everapi.server.player.EPlayer;
@@ -13,5 +15,9 @@ public interface BossBarService {
 	public boolean remove(EPlayer player, String identifier);
 
 	public boolean remove(EPlayer player, int priority);
+
+	public Optional<ServerBossBar> get(EPlayer ePlayer, String identifier);
+	
+	public Optional<ServerBossBar> get(EPlayer ePlayer, int priority);
 	
 }
