3cefffebd4ae5cad1030011cce1ff2a0e909e749
rexbut <butbutdu45@gmail.com>
2016-06-22 01:37:58 +0200

    Update License
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/everapi/services/actionbar/ActionBarMessage.java
+++ src/main/java/fr/evercraft/everapi/services/actionbar/ActionBarMessage.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with EverAPI.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 /**
  * This file is part of EverAPI.
  *
