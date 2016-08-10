e849c2f0bb52a2d8e78276352cae7fb87bb97df7
rexbut <butbutdu45@gmail.com>
2016-08-10 08:42:14 +0200

    Update
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/everapi/services/signs/SignSubject.java
+++ src/main/java/fr/evercraft/everapi/services/signs/SignSubject.java
@@ -19,6 +19,7 @@ package fr.evercraft.everapi.services.signs;
 import java.util.List;
 
 import org.spongepowered.api.block.tileentity.Sign;
+import org.spongepowered.api.data.manipulator.mutable.tileentity.SignData;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColor;
 import org.spongepowered.api.text.format.TextColors;
@@ -29,15 +30,15 @@ import fr.evercraft.everapi.server.player.EPlayer;
 
 public interface SignSubject {
 	
-	public static final TextColor COLOR_ENABLE = TextColors.BLUE;
+	public static final TextColor COLOR_ENABLE = TextColors.DARK_BLUE;
 	public static final TextColor COLOR_DISABLE = TextColors.RED;
 	
 	public String getName();
 	
-	public boolean create(EPlayer player, Sign sign);
+	public boolean create(EPlayer player, Location<World> location, SignData data);
 	public boolean useEnable(EPlayer player, Sign sign);
 	public boolean useDisable(EPlayer player, Sign sign);
-	public boolean remove(EPlayer player, Location<World> location, final List<Text> sign);
+	public boolean remove(EPlayer player, Location<World> location, List<Text> sign);
 	
 	public boolean valide(Sign sign);
 }
