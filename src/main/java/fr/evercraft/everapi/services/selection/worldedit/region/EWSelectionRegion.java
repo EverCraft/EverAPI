04348be8887a3f98cdce381f4621429314b79117
rexbut <butbutdu45@gmail.com>
2017-06-03 13:50:19 +0200

    Update

--- src/main/java/fr/evercraft/everapi/services/selection/worldedit/region/EWSelectionRegion.java
+++ src/main/java/fr/evercraft/everapi/services/selection/worldedit/region/EWSelectionRegion.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of EverAPI.
+ *
+ * EverAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EverAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EverAPI.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.evercraft.everapi.services.selection.worldedit.region;
 
 import java.util.ArrayList;
@@ -76,7 +92,7 @@ public class EWSelectionRegion implements SelectionRegion {
 				vectors.add(Vector3i.from(vector.getBlockX(), min, vector.getBlockZ()));
 			}
 			
-			Vector3i last = vectors.get(vectors.size());
+			Vector3i last = vectors.get(vectors.size()-1);
 			vectors.set(vectors.size()-1, Vector3i.from(last.getX(), max, last.getZ()));
 			return vectors;
 		}
